598bd5ed4311ec19eeb9c6252bbe24d61dbef8cb
Sarah <sarahalr@tamu.edu>
2024-10-18 18:21:20 -0500

    Changed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
+//setting up express js server
+
 const express = require('express')
+const colors = require('colors')
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
 
-const app = express()
+connectDB() //connect to the database
 
-app.length('/api/goals', (req,res))
-app.listen(port, () => console.log(`Server started on port ${port}`))
+const app = express() //initialize express
+
+app.use(express.json()) //middleware to parse json
+app.use(express.urlencoded({extended: false})) //middleware to parse urlencoded data
+
+app.use('/api/users', require('./routes/userRoutes')) //use the goalRoutes.js file for any routes that start with /api/goals
+
+app.use(errorHandler) //use the error handler middleware and overwrite the default error handler
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
