03cc751d401baf18a608242b2a677a3857ede61d
Sophia Phu <sophiatiffphu@gmail.com>
2024-10-18 22:43:16 -0500

    fixing merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from './pages/login'
-
+import UserDashboard from './pages/UserDashboard'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/user-dashboard" element={<UserDashboard/>}/>
       </Routes>
     </BrowserRouter>
   )
