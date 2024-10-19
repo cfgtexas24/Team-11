adf612d832ed93bb28267f8ec4dd562032e561d2
alissapark <alissapark24@gmail.com>
2024-10-19 02:42:08 -0500

    reformatted schedule appointment

--- frontend/src/components/userPageComponents/ViewLabReports.tsx
+++ frontend/src/components/userPageComponents/ViewLabReports.tsx
@@ -1,24 +1,44 @@
-import * as React from "react"
-import '../../App.css'
+import * as React from "react";
+import '../../App.css';
+import { IoIosDownload } from "react-icons/io"; // Import the download icon
 
-function ViewLabReports() {
-    const [date, setDate] = React.useState<Date | undefined>(new Date())
+const ViewLabReports: React.FC = () => {
+  const handleDownload = (fileName: string) => {
+    const link = document.createElement('a');
+    link.href = `/lab_reports/${fileName}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <>
-      <div className="justify-around rounded-lg shadow-md" style={{ backgroundColor: '#F0DCDA' }}>
+      <div className="justify-around rounded-lg shadow-md h=300px" style={{ backgroundColor: '#F0DCDA' }}>
         <div className="flex flex-row">
-            <div className="text-left">
-                <h1>View Lab Reports</h1>
-                <div>
-                    will update with lab reports
-                </div>
+          <div className="text-left">
+            <h1>View Lab Reports</h1>
+            <div>
+              <div 
+                onClick={() => handleDownload('lab_report_1.pdf')} 
+                style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}
+              >
+                Ultrasound Lab Results 07/30/2024
+                <IoIosDownload style={{ marginRight: '8px' }} />
+              </div>
+              <div 
+                onClick={() => handleDownload('lab_report_2.pdf')} 
+                style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}
+              >
+                Blood Count Sample Report 02/10/2014
+                <IoIosDownload style={{ marginRight: '8px' }} />
+              </div>
             </div>
+          </div>
         </div>
-
       </div>
     </>
-  )
+  );
 }
 
-export default ViewLabReports
+export default ViewLabReports;
--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -10,21 +10,21 @@ function UserDashboard() {
         <>
             <div className="h-screen w-screen absolute left-0" style={{ backgroundColor: '#F7EFEE' }}>
                 <div className="flex flex-wrap justify-between"> 
-                    <div className="w-full md:w-1/2 p-2"> 
+                    <div className="w-full h-full md:w-1/2 p-2 flex-grow"> 
                         <ViewAppointments />
                     </div>
-                    <div className="w-full md:w-1/2 p-2">
+                    <div className="w-full h-full md:w-1/2 p-2 flex-grow">
                         <ScheduleAppointments />
                     </div>
                 </div>
                 <div className="flex flex-wrap justify-between mt-4"> 
-                    <div className="w-full md:w-1/3 p-2"> 
+                    <div className="w-full h-full md:w-1/3 p-2 flex-grow"> 
                         <ViewLabReports />
                     </div>
-                    <div className="w-full md:w-1/3 p-2">
+                    <div className="w-full h-full md:w-1/3 p-2 flex-grow">
                         <EducationalServices />
                     </div>
-                    <div className="w-full md:w-1/3 p-2"> 
+                    <div className="w-full h-full md:w-1/3 p-2 flex-grow"> 
                         <Feedback />
                     </div>
                 </div>
