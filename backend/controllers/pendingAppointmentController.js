ab2fc464da24cc5db1296351bca2cd0bdb77395b
charuy2 <109783658+charuy2@users.noreply.github.com>
2024-10-19 03:41:07 -0500

    edited and tested get appointments method

--- backend/controllers/pendingAppointmentController.js
+++ backend/controllers/pendingAppointmentController.js
@@ -1,48 +1,48 @@
-const asyncHandler = require('express-async-handler');
-const Appointment = require('../models/pendingAppointmentModel');
-const User = require('../models/userModel');
+// const asyncHandler = require('express-async-handler');
+// const Appointment = require('../models/pendingAppointmentModel');
+// const User = require('../models/userModel');
 
-const setAppointmentPending = asyncHandler(async (req, res) => {
-    try {
-        const { userId, clinicName, date, time, physician, type, valid } = req.body;
+// const setAppointmentPending = asyncHandler(async (req, res) => {
+//     try {
+//         const { userId, clinicName, date, time, physician, type, valid } = req.body;
 
-        // Create a new appointment
-        const appointment = await Appointment.create({
-            userId,
-            clinicName,
-            date,
-            time,
-            physician,
-            type,
-            valid
-        });
+//         // Create a new appointment
+//         const appointment = await Appointment.create({
+//             userId,
+//             clinicName,
+//             date,
+//             time,
+//             physician,
+//             type,
+//             valid
+//         });
 
-        // Send back the created appointment info including its ID
-        res.status(201).json({
-            _id: appointment._id,
-            userId: appointment.userId,
-            clinicName: appointment.clinicName,
-            time: appointment.time,
-            physician: appointment.physician,
-            type: appointment.type,
-            valid: appointment.valid
-        });
-    } catch (error) {
-        console.error("Error creating appointment:", error);
-        res.status(500).json({ message: 'Failed to create appointment' });
-    }
-});
+//         // Send back the created appointment info including its ID
+//         res.status(201).json({
+//             _id: appointment._id,
+//             userId: appointment.userId,
+//             clinicName: appointment.clinicName,
+//             time: appointment.time,
+//             physician: appointment.physician,
+//             type: appointment.type,
+//             valid: appointment.valid
+//         });
+//     } catch (error) {
+//         console.error("Error creating appointment:", error);
+//         res.status(500).json({ message: 'Failed to create appointment' });
+//     }
+// });
 
-const makeAppointment = asyncHandler(async (req, res) => {
-    const { clinicName, date, time, physician, type} = req.body;
-})
-
-
+// const makeAppointment = asyncHandler(async (req, res) => {
+//     const { clinicName, date, time, physician, type} = req.body;
+// })
 
 
 
 
 
-module.exports = {
-    setAppointmentPending
-};
+
+
+// module.exports = {
+//     setAppointmentPending
+// };
--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -9,10 +9,5 @@ router.post('/:userId/appointments', setAppointment);
 // Route to get appointments for a user
 router.get('/:userId/appointments', getAppointments);
 
-<<<<<<< HEAD
-
 
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 06c3916380e5f995d0076753eb284ee83a51f198
