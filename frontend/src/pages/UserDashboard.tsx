4ae97d31b6a1ba09a8117ef1351f6d134dcb459d
alissapark <alissapark24@gmail.com>
2024-10-19 03:46:12 -0500

    educational service component

--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -8,7 +8,7 @@ import Feedback from '@/components/userPageComponents/Feedback';
 function UserDashboard() {
     return (
         <>
-            <div className="h-screen w-screen absolute left-0" style={{ backgroundColor: '#F7EFEE' }}>
+            <div className="h-screen w-screen absolute left-0 top-30px bg-[#F7EFEE]">
                 <div className="flex flex-wrap justify-between"> 
                     <div className="w-full h-full md:w-1/2 p-2 flex-grow"> 
                         <ViewAppointments />
@@ -18,13 +18,13 @@ function UserDashboard() {
                     </div>
                 </div>
                 <div className="flex flex-wrap justify-between mt-4"> 
-                    <div className="w-full h-full md:w-1/3 p-2 flex-grow"> 
+                    <div className="w-full h-full md:w-4/12 p-2 flex-grow"> 
                         <ViewLabReports />
                     </div>
-                    <div className="w-full h-full md:w-1/3 p-2 flex-grow">
+                    <div className="w-full h-full md:w-5/12 p-2 flex-grow">
                         <EducationalServices />
                     </div>
-                    <div className="w-full h-full md:w-1/3 p-2 flex-grow"> 
+                    <div className="w-full h-full md:w-3/12 p-2 flex-grow"> 
                         <Feedback />
                     </div>
                 </div>
