772d725550f02df79269a97a39ebcd0d9a5e8798
charuy2 <109783658+charuy2@users.noreply.github.com>
2024-10-18 21:48:05 -0500

    updated appointment.js

--- backend/models/apointmentModel.js
+++ backend/models/apointmentModel.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose'); //to interact w mongod
 //fields the user model will have
 const apointmentModel = mongoose.Schema({
-    appointmentType: {
+    clinicName: {
         type: String,
         required: true,
     },
     date: {
         type: Date,
         required: true
-    }
+    },
+    time: {
+        type: Date,  
+        required: true,
+    },
+    physician: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['prenatal care', 'checkup', 'follow-up'], // Dropdown options
+        required: true,
+    },
 },{
     timestamps: true //adds created at and updated at fields
 })
