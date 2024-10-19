1deef71a3de241c81992d919d1f0383df1d5e33a
alissapark <alissapark24@gmail.com>
2024-10-18 22:59:03 -0500

    basic view appointments component

---
+++ frontend/src/components/ViewAppointments.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+import '../App.css'
+import { Calendar } from "@/components/ui/calendar"
+
+function ViewAppointments() {
+    const [date, setDate] = React.useState<Date | undefined>(new Date())
+
+  return (
+    <>
+      <div className="justify-around w-1/2 h-auto md:w-1/2 md:h-1/2" style={{ backgroundColor: '#F0DCDA' }}>
+        <div className="flex flex-row">
+            <div className="text-left">
+                <h1>Your Appointments</h1>
+                <h3>Upcoming Appointments</h3>
+                <div>
+                    will fill with appointments
+                </div>
+            </div>
+            <div>
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-md border"
+                />
+            </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default ViewAppointments
--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import '../App.css'
+import ViewAppointments from '@/components/ViewAppointments'
 
 function UserDashboard() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="h-screen w-screen" style={{ backgroundColor: '#F0DCDA' }}>
+      <div className="h-screen w-screen" style={{ backgroundColor: '#F7EFEE' }}>
         <p>Dashboard</p>
+        <ViewAppointments/>
 
       </div>
     </>
