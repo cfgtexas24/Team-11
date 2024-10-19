9699137cc8376516ebaa78440c3eae0a89c0589f
Sophia Phu <sophiatiffphu@gmail.com>
2024-10-18 22:44:13 -0500

    revert back

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from './pages/login'
-import UserDashboard from './pages/UserDashboard'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/user-dashboard" element={<UserDashboard/>}/>
+        <Route path="/" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
