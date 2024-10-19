a37bf0258dfdf62f5cb077707669118b1a2b1c8e
alissapark <alissapark24@gmail.com>
2024-10-19 00:45:38 -0500

    added navbar to user dashboard

--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -4,27 +4,32 @@ import ScheduleAppointments from '@/components/userPageComponents/ScheduleAppoin
 import ViewLabReports from '@/components/userPageComponents/ViewLabReports';
 import EducationalServices from '@/components/userPageComponents/EducationalServices';
 import Feedback from '@/components/userPageComponents/Feedback';
+import Navbar from '@/components/navbar';
 
 function UserDashboard() {
 
   return (
     <>
       <div className="h-screen w-screen" style={{ backgroundColor: '#F7EFEE' }}>
-        <p>dashboard</p>
         <div>
-          <ViewAppointments/>
+          <Navbar />
         </div>
-        <div>
-          <ScheduleAppointments/>
-        </div>
-        <div>
-          <ViewLabReports />
-        </div>
-        <div>
-          <EducationalServices />
-        </div>
-        <div>
-          <Feedback />
+        <div className="flex flex-col gap-4 mt-4">
+          <div>
+            <ViewAppointments/>
+          </div>
+          <div>
+            <ScheduleAppointments/>
+          </div>
+          <div>
+            <ViewLabReports />
+          </div>
+          <div>
+            <EducationalServices />
+          </div>
+          <div>
+            <Feedback />
+          </div>
         </div>
       </div>
     </>
