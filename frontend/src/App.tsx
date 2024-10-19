67da4f70332bf1aa8a272a00976959d8fb30128e
alissapark <alissapark24@gmail.com>
2024-10-18 22:15:33 -0500

    added user dashboard page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from './pages/login'
+import UserDashboard from './pages/UserDashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login/>} />
+        <Route path="/user-dashboard" element={<UserDashboard/>}/>
       </Routes>
     </BrowserRouter>
   )
